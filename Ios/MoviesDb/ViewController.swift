9ddcf91121a0733a1d18ab3559432ae00b23ab5d
Rodrigo <rodrigolmti@gmail.com>
2016-03-15 12:54:00 -0300

    TableView and functions

--- Ios/MoviesDb/ViewController.swift
+++ Ios/MoviesDb/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         } else {
             self.alert(NSLocalizedString("Ops .:.", comment: ""), message: NSLocalizedString("Please put the name of a movie!", comment: ""))
         }
-        
     }
     
     override func viewDidLoad() {        
@@ -37,107 +36,62 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     override func viewDidAppear(animated: Bool) {
-
-        let fetchRequest = NSFetchRequest(entityName: "Movies")
-        let context:NSManagedObjectContext = appDel.managedObjectContext
-        
-        do {
-            let results = try context.executeFetchRequest(fetchRequest)
-            self.allMovies = results as! [DataMovies]
-            self.tableViewMovie.reloadData()
-            //print(self.allMovies)
-        } catch let error as NSError {
-            print("Could not fetch \(error), \(error.userInfo)")
-        }
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        self.allMovies.removeAll()
+        self.loadObject()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
     }
-    
-//    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return "last added movies"
-//    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return allMovies.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("customCell", forIndexPath: indexPath) as! CustomCell
+    func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customCell") as! CustomCell
         
         var movieRow = createObject()
         movieRow = self.allMovies[indexPath.row]
         
         cell.titleLB.text = movieRow.title
         cell.genreLB.text = movieRow.genre
-        
+            
         if ((movieRow.baseimg?.isEmpty) != nil) {
             cell.imageIMG.nk_setImageWith(NSURL(string: movieRow.baseimg!)!)
         }
-        
         return cell
     }
     
     // Request JSON on OMDB
-    
     func getMovie(query: String) {
-        
-        let context:NSManagedObjectContext = appDel.managedObjectContext
-        let ent = NSEntityDescription.entityForName("Movies", inManagedObjectContext: context)
-        let movies = DataMovies(entity: ent!, insertIntoManagedObjectContext:context)
-        
+        let movies = createObject()
+
         if NetworkRechability.isConnectedToNetwork() == true {
-        
                 let baseUrl: String = "http://www.omdbapi.com/?t=\(query)&tomatoes=true&type=movie"
-                        
                 Alamofire.request(.GET, baseUrl).validate().responseJSON { response in
                     switch response.result {
                     case .Success:
                         if let value = response.result.value {
                             let json = JSON(value)
+                            print(json.description)
                             let error = json["Error"].string
                             
                             if(error == nil) {
-                                if (json["imdbID"].string! != movies.imdbid){
+                                movies.imdbid = json["imdbID"].string
+                                movies.title = json["Title"].string!
+                                movies.genre = json["Genre"].string!
+                                movies.director = json["Director"].string!
+                                movies.plot = json["Plot"].stringValue
+                                movies.rating = json["imdbRating"].string!
+                                movies.actors = json["Actors"].stringValue
+                                movies.baseimg = json["Poster"].stringValue
                                     
-                                    movies.imdbid = json["imdbID"].string
-                                    movies.title = json["Title"].string!
-                                    movies.genre = json["Genre"].string!
-                                    movies.director = json["Director"].string!
-                                    movies.plot = json["Plot"].stringValue
-                                    movies.rating = json["imdbRating"].string!
-                                    movies.actors = json["Actors"].stringValue
-                                    movies.baseimg = json["Poster"].stringValue
-                                    
-                                    self.allMovies.append(movies)
-                                    self.tableViewMovie.reloadData()
-                                    
-                                    do {
-                                        try context.save()
-                                    } catch {
-                                        fatalError("Failure to save context: \(error)")
-                                    }
-
-                                    print("Object saved!")
-                                    print("Object: ")
-                                    print(movies)
-                                    
-                                    
-                                } else {
-                                    self.alert(NSLocalizedString("Movies already added", comment: ""), message: NSLocalizedString("This movies was already added", comment: ""))
-                                }
+                                self.saveObject(movies)
                             } else {
-                               self.alert(NSLocalizedString("Ops, Something Wrong", comment: ""), message: NSLocalizedString(error!, comment: ""))
+                               self.alert(NSLocalizedString("Ops, Something Wrong", comment: ""), message: NSLocalizedString("Try again", comment: ""))
                             }
                         }
                     case .Failure(let error):
-                      
                         print(error)
                     }
                 }
@@ -147,15 +101,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    //Crete object
     func createObject() -> DataMovies {
         let context:NSManagedObjectContext = appDel.managedObjectContext
         let ent = NSEntityDescription.entityForName("Movies", inManagedObjectContext: context)
         let movies = DataMovies(entity: ent!, insertIntoManagedObjectContext:context)
         return movies
     }
+    //Save object
+    func saveObject(movie : DataMovies) {
+        let context:NSManagedObjectContext = appDel.managedObjectContext
+        do {
+            try context.save()
+            self.allMovies.append(movie)
+        } catch {
+            fatalError("Failure to save context: \(error)")
+        }
+        self.tableViewMovie.reloadData()
+    }
+    
+    //Load object
+    func loadObject() {
+        let fetchRequest = NSFetchRequest(entityName: "Movies")
+        let context:NSManagedObjectContext = appDel.managedObjectContext
+        do {
+            let results = try context.executeFetchRequest(fetchRequest)
+            self.allMovies = results as! [DataMovies]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        self.tableViewMovie.reloadData()
+    }
     
     // Detail segue
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let indexPath = self.tableViewMovie?.indexPathForCell(sender as! UITableViewCell) {
             let detailVC = segue.destinationViewController as! MovieDetailController
@@ -168,12 +146,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             detailVC.rating = currentMovie.rating
             detailVC.actors = currentMovie.actors
             detailVC.genre = currentMovie.genre
-
         }
     }
     
     // Alert Errors
-    
     func alert(title: String, message: String) {
         if let _: AnyClass = NSClassFromString("UIAlertController") {
             let myAlert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
